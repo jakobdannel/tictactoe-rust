96d0a3bb1c4bd070a823efea37e7b2bb19436bf9
jakobdannel <jakob.dannel@t-online.de>
2022-10-07 16:51:51 +0200

    Changed method for vert and horiz, method for >3

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-#[derive(Debug)]
-#[derive(PartialEq)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 enum Field {
     X,
     O,
@@ -17,44 +16,18 @@ fn winner(board: Vec<Vec<Field>>) -> Field {
     let mut winner: Field = Field::Empty;
     let mut count_x;
     let mut count_o;
-    for x in 0..3 {
-        count_x = 0;
-        count_o = 0;
-        for y in 0..3 {
-            if board[x][y] == Field::X {
-                count_x += 1;
-            }
-            if board[x][y] == Field::O {
-                count_o += 1;
-            }
+    //Checks for vertical and horizontal wins
+    for x in 0 .. 3 {
+        if board[x][0] == board[x][1] && board[x][1] == board[x][2] {
+            return board[x][0];
         }
-        if count_x == 3 {
-            winner = Field::X;
-        }
-        if count_o == 3 {
-            winner = Field::O;
-        }
-    }
-    for y in 0..3 {
-        count_x = 0;
-        count_o = 0;
-        for x in 0..3 {
-            if board[x][y] == Field::X {
-                count_x += 1;
-            }
-            if board[x][y] == Field::O {
-                count_o += 1;
-            }
-        }
-        if count_x == 3 {
-            winner = Field::X;
-        }
-        if count_o == 3 {
-            winner = Field::O;
+        if board[0][x] == board[1][x] && board[1][x] == board[2][x] {
+            return board[0][x];
         }
     }
     count_x = 0;
     count_o = 0;
+    //Checks for diagonal wins from top left to bottom right
     for i in 0..3 {
         
         if board[i][i] == Field::X {
@@ -65,12 +38,8 @@ fn winner(board: Vec<Vec<Field>>) -> Field {
         }
         
     }
-    if count_x == 3 {
-        winner = Field::X;
-    }
-    if count_o == 3 {
-        winner = Field::O;
-    }
+    winner = morethanthree(count_x, count_o, winner);
+    //Checks for diagonal wins from top right to bottom left
     count_x = 0;
     count_o = 0;
     for i in 0..3 { 
@@ -81,6 +50,12 @@ fn winner(board: Vec<Vec<Field>>) -> Field {
             count_o += 1;
         } 
     }
+    winner = morethanthree(count_x, count_o, winner);
+    winner
+}
+
+fn morethanthree(count_x: u32, count_o: u32, previous_winner: Field) -> Field{
+    let mut winner: Field = previous_winner;
     if count_x == 3 {
         winner = Field::X;
     }
